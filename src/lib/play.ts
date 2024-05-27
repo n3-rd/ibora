7e9a91c5d32d42e27a904f40ba24cf45e893084d
n3-rd <godwinjdayo@gmail.com>
2024-05-27 01:21:21 +0100

    play sounds

---
+++ src/lib/play.ts
@@ -0,0 +1,31 @@
+import { Howl } from 'howler';
+import { sounds } from './sounds';
+
+export function play(id: string) {
+	const sound = sounds.find((sound) => sound.id === id);
+	if (!sound) {
+		console.error('Sound not found:', id);
+		return;
+	}
+	if (!sound.howl) {
+		sound.howl = new Howl({
+			src: [sound.src],
+			loop: true,
+			html5: true,
+			volume: sound.volume
+		});
+	}
+	if (sound.howl) {
+		(sound.howl as Howl).play();
+	}
+}
+export function stop(id: string) {
+	const sound = sounds.find((sound) => sound.id === id);
+	if (!sound) {
+		console.error('Sound not found:', id);
+		return;
+	}
+	if (sound.howl) {
+		(sound.howl as Howl).stop(); // Add type assertion to access the 'stop' method
+	}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { sounds } from '$lib/sounds';
+	import { Toggle } from '$lib/components/ui/toggle/index.js';
+	import { play, stop } from '$lib/play';
+</script>
+
+{#each sounds as sound}
+	<Toggle
+		aria-label="toggle bold"
+		onPressedChange={(e) => {
+			console.log(e);
+			if (e == true) {
+				play(sound.id);
+			} else {
+				stop(sound.id);
+			}
+		}}
+	>
+		{sound.id}
+	</Toggle>
+{/each}
