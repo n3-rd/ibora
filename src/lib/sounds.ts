32924ea1c1098ffff2354f4ae5ff546c2c5e686f
n3-rd <godwinjdayo@gmail.com>
2024-05-27 01:07:39 +0100

    initial

---
+++ src/lib/sounds.ts
@@ -0,0 +1,16 @@
+export const sounds = [
+    { id: 'rain', src: '/sounds/rain.ogg', howl: null, volume: 0.5 },
+    { id: 'birds', src: '/sounds/birds.ogg', howl: null, volume: 0.5 },
+    { id: 'coffee-shop', src: '/sounds/coffee-shop.ogg', howl: null, volume: 0.5 },
+    { id: 'summer-night', src: '/sounds/summer-night.ogg', howl: null, volume: 0.5 },
+    { id: 'white-noise', src: '/sounds/white-noise.ogg', howl: null, volume: 0.5 },
+    { id: 'boat', src: '/sounds/boat.ogg', howl: null, volume: 0.5 },
+    { id: 'fireplace', src: '/sounds/fireplace.ogg', howl: null, volume: 0.5 },
+    { id: 'storm', src: '/sounds/storm.ogg', howl: null, volume: 0.5 },
+    { id: 'train', src: '/sounds/train.ogg', howl: null, volume: 0.5 },
+    { id: 'wind', src: '/sounds/wind.ogg', howl: null, volume: 0.5 },
+    { id: 'city', src: '/sounds/city.ogg', howl: null, volume: 0.5 },
+    { id: 'pink-noise', src: '/sounds/pink-noise.ogg', howl: null, volume: 0.5 },
+    { id: 'stream', src: '/sounds/stream.ogg', howl: null, volume: 0.5 },
+    { id: 'waves', src: '/sounds/waves.ogg', howl: null, volume: 0.5 }
+]
